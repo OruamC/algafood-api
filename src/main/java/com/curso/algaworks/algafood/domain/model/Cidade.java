34fba2c8e738a4612786423bf8db0f538f29f6a2
Oruam <oru_catani@hotmail.com>
2021-03-23 21:16:04 -0300

    Criando uma anotacao customizada

--- src/main/java/com/curso/algaworks/algafood/domain/model/Cidade.java
+++ src/main/java/com/curso/algaworks/algafood/domain/model/Cidade.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
-import com.curso.algaworks.algafood.api.Groups;
+import com.curso.algaworks.algafood.core.validation.Groups;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
