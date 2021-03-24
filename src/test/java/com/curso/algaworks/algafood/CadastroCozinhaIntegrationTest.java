e81c23190dc7bb0e9b6f978314459e7ee47cc9a8
Oruam <oru_catani@hotmail.com>
2021-03-24 16:56:20 -0300

    Iniciando testes de cozinha

---
+++ src/test/java/com/curso/algaworks/algafood/CadastroCozinhaIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.curso.algaworks.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.curso.algaworks.algafood.domain.model.Cozinha;
+import com.curso.algaworks.algafood.domain.service.CadastroCozinhaService;
+
+@SpringBootTest
+class CadastroCozinhaIntegrationTest {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+	
+	@Test
+	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+		
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
+	@Test()
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+		
+		assertThrows(ConstraintViolationException.class, () -> cadastroCozinha.salvar(novaCozinha));
+	}
+
+}
