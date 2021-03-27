15524fdfaa99628f0440ae52e7d0de983b15a3e6
Oruam <oru_catani@hotmail.com>
2021-03-27 16:15:15 -0300

    Deixando scripts sql genericos em relacao ao banco que ira usar

--- src/main/resources/db/migration/V003__cria-tabela-estado.sql
+++ src/main/resources/db/migration/V003__cria-tabela-estado.sql
@@ -1,20 +1,20 @@
-create table algafood.estado(
+create table estado(
 	id bigint not null auto_increment,
 	nome varchar(80) not null,
     
     primary key (id)
 ) engine=InnoDB default charset=utf8;
 
-insert into algafood.estado (nome) select distinct 
-algafood.cidade.nome_estado from algafood.cidade;
+insert into estado (nome) select distinct 
+cidade.nome_estado from cidade;
 
-alter table algafood.cidade add column estado_id bigint not null;
+alter table cidade add column estado_id bigint not null;
 
-update algafood.cidade set algafood.cidade.estado_id = (select algafood.estado.id from algafood.estado where algafood.estado.nome = algafood.cidade.nome_estado);
+update cidade set cidade.estado_id = (select estado.id from estado where estado.nome = cidade.nome_estado);
 
-alter table algafood.cidade add constraint fk_cidade_estado
-foreign key (estado_id) references algafood.estado (id);
+alter table cidade add constraint fk_cidade_estado
+foreign key (estado_id) references estado (id);
 
-alter table algafood.cidade drop column nome_estado;
+alter table cidade drop column nome_estado;
 
-alter table algafood.cidade change nome_cidade nome varchar(80) not null;
+alter table cidade change nome_cidade nome varchar(80) not null;
