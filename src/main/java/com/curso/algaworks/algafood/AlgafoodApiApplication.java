9c11543a14c2c0f7feb53c1979e600af5939d9eb
Oruam <oru_catani@hotmail.com>
2021-03-30 21:11:45 -0300

    Configurando projeto para utlizar padrao UTC

--- src/main/java/com/curso/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/curso/algaworks/algafood/AlgafoodApiApplication.java
@@ -6,11 +6,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.curso.algaworks.algafood.infrastructure.repository.CustomJpaRepositoryImpl;
 
+import java.sql.Time;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
 		SpringApplication.run(AlgafoodApiApplication.class, args);
 	}
 
